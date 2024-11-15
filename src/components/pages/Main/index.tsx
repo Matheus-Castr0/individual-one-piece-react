e247eae4215f6715954d16df5b3af4ad77e389e1
matheuscastro <matheus.csilva@sptech.school>
2024-11-15 19:45:26 -0300

    feat: hook para definir titulo de pagina

--- src/components/pages/Main/index.tsx
+++ src/components/pages/Main/index.tsx
@@ -1,4 +1,8 @@
+import useTitlePage from "../../../hooks/useTitlePage"
+
 export default function Main() {
+    useTitlePage("One Piece - Home");
+
     return(
         <>
             <h1>Olá, mundo</h1>
